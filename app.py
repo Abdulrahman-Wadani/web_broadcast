52dd21471d65b8dd2ac71a5fa33becc9a85529a1
Abdulrhman S <ABDULRHMAN.SALAMAH0@GMAIL.COM>
2025-11-17 21:47:24 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
-model.add(Dense(20, activation='softmax'))
+model.add(Dense(3, activation='softmax'))
 
 print(os.getcwd())
 
