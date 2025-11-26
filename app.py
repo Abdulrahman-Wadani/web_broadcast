78efc94a984e775235425e2ea13ed40244b40879
Abdulrhman S <ABDULRHMAN.SALAMAH0@GMAIL.COM>
2025-11-26 10:12:43 +0300

    Adding latests model and adjusting the activation function for the LSTM neural network

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ socketio = SocketIO(
 # Recreate the model architecture before loading weights
 
 model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(LSTM(64, return_sequences=True, activation='tanh', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='tanh'))
+model.add(LSTM(64, return_sequences=False, activation='tanh'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(10, activation='softmax'))
